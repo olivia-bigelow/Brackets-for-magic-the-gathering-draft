b9e1e073c70d7be387a97492a0858822e1a615c7
olivia-bigelow <divinthezabrak@gmail.com>
2022-12-16 12:09:42 -0700

    Add project files.

---
+++ Brackets/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //add players
+            List<string> players = new();
+            players.Add("Liv");
+            players.Add("Tasha");
+            players.Add("Matt");
+            players.Add("Jackson");
+            players.Add("David");
+            players.Add("Adrian");
+            players.Add("Lorens");
+            players.Add("placeholder");
+
+            //rearange
+            printBracket(players);
+        }
+
+        public static void printBracket<T>(List<T> list) where T : notnull
+        {
+            //intialize random
+            Random rand = new();
+
+            //rearrange
+            for (int i = 0; i < 1000; i++)
+            {
+                //get a temp random
+                int playerToMove = rand.Next(list.Count);
+                while (i % list.Count == playerToMove)
+                {
+                    //if the 2 values are the same, change it until theyre not
+                    playerToMove = rand.Next(list.Count);
+                }
+
+                T p1 = list[playerToMove];
+                list[playerToMove] = list[i % list.Count];
+                list [i % list.Count] = p1;
+            }
+
+            //once the list is rearranged, display the brakect
+            for (int i = 0; i < list.Count; i++)
+                Console.WriteLine((i + 1) + ": " + list[i]);
+        }
+    }
+}
---
+++ brakets/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.VisualBasic;
+
+//add players
+List<string> players = new();
+players.Add("Liv");
+players.Add("Tasha");
+players.Add("Matt");
+players.Add("Jackson");
+players.Add("David");
+players.Add("Adrian");
+players.Add("Lorens");
+players.Add("placeholder");
+
+//intialize random
+Random rand = new();
+
+//rearrange
+for(int i = 0; i < 1000; i++)
+{
+    //get a temp random
+    int playerToMove = rand.Next(8);
+    while(i%8 == playerToMove)
+    {
+        //if the 2 values are the same, change it until theyre not
+        playerToMove = rand.Next(8);
+    }
+
+    string p1 = players[playerToMove];
+    players[playerToMove] = players[i%8];
+    players[i%8] = p1;
+}
+
+//once the list is rearranged, display the brakect
+for (int i = 0; i < players.Count; i++)
+    Console.WriteLine((i+1) + ": " + players[i]);
